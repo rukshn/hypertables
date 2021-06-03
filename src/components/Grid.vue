46c6f1eae9de8bfb648fd57f4a03dd18eaf6452e
Rukshan <arkruka@gmail.com>
2021-06-03 10:44:08 +0530

    Adde arrow key navigation to the grid

--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -4,7 +4,7 @@
       <div class="w-1/6" v-for="(column, colIndex) in columns" :key="colIndex" :ref="bindCol(colIndex)">
         <div class="bg-gray-200 text-center py-1 px-2 w-full">{{colIndex}}</div>
         <div class="w-full" v-for="(row, rowIndex) in rows" :key="rowIndex">
-          <input @keydown.enter="enterCell" :data-col="colIndex" :data-row="rowIndex" @keydown.tab="tabCell" :ref="bindRow(rowIndex, colIndex)" type="text" class="focus:outline-none focus:bg-green-100 focus:border-gray-200 py-1 px-1 border border-gray-100 min-w-full">
+          <input @keydown.down="cellDown" @keydown.up="cellUp" @keydown.left="cellLeft" @keydown.right="cellRight" @keydown.enter="enterCell" :data-col="colIndex" :data-row="rowIndex" @keydown.tab="tabCell" :ref="bindRow(rowIndex, colIndex)" type="text" class="focus:outline-none focus:bg-green-100 focus:border-gray-200 py-1 px-1 border border-gray-100 min-w-full">
         </div>
       </div>
     </div>
@@ -48,6 +48,46 @@ export default defineComponent({
       const newRow:number = row
       const newCol:number = +col + 1
 
+      const ref:string = `col-${newCol}-row-${newRow}`
+      if(this.$refs[ref]) this.$refs[ref].focus()
+    },
+    cellDown(e:any) {
+      e.preventDefault()
+      const col:number = e.target.getAttribute('data-col')
+      const row:number = e.target.getAttribute('data-row')
+
+      const newRow:number = +row + 1
+      const newCol:number = col
+      const ref:string = `col-${newCol}-row-${newRow}`
+      if(this.$refs[ref]) this.$refs[ref].focus()
+    },
+    cellUp(e:any) {
+      e.preventDefault()
+      const col:number = e.target.getAttribute('data-col')
+      const row:number = e.target.getAttribute('data-row')
+
+      const newRow:number = +row - 1
+      const newCol:number = col
+      const ref:string = `col-${newCol}-row-${newRow}`
+      if(this.$refs[ref]) this.$refs[ref].focus()
+    },
+    cellLeft(e:any) {
+      e.preventDefault()
+      const col:number = e.target.getAttribute('data-col')
+      const row:number = e.target.getAttribute('data-row')
+
+      const newRow:number = row
+      const newCol:number = +col - 1
+      const ref:string = `col-${newCol}-row-${newRow}`
+      if(this.$refs[ref]) this.$refs[ref].focus()
+    },
+    cellRight(e:any) {
+      e.preventDefault()
+      const col:number = e.target.getAttribute('data-col')
+      const row:number = e.target.getAttribute('data-row')
+
+      const newRow:number = row
+      const newCol:number = +col + 1
       const ref:string = `col-${newCol}-row-${newRow}`
       if(this.$refs[ref]) this.$refs[ref].focus()
     }
