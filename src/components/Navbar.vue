648e6e609b5bb25ca74e6692f760b427e0732053
Rukshan <arkruka@gmail.com>
2021-06-06 22:34:56 +0530

    Navbar initital commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="navbar select-none bg-blue-500 py-2 px-5 flex justify-center">
+    <h1 class="text-white font-bold flex items-center">HyperTables⚡️</h1>
+    <h1 class="text-white font-bold flex items-center text-lg flex-grow content-center">Table Name</h1>
+    <div class="buttons flex content-end items-center w-1/12">
+      <a class="bg-transparent flex items-center space-x-2 text-sm text-blue-100 font-semibold hover:text-blue-50 uppercase" href="/help"
+        target="_blank"><span>help</span>
+        <svg width="16" height="16" viewBox="0 0 16 16" class="icon" style="shape-rendering: geometricPrecision;"><path fill-rule="evenodd" fill="currentColor" d="M8.7589721,9.5 C8.7589721,9.77614237 8.5432876,10 8.25962632,10 L7.49934584,10 C7.22356477,10 7.00000005,9.76806641 7.00000005,9.5 L7.00000005,9 C7.00000005,8.34869385 9.25010007,7.05957031 9.2501,6 C9.25009998,5.652 9.0251,4.75 8.0001,4.75 C6.9791,4.75 6.7881,5.734 6.7551,6 C6.7551,6 5.26889746,6.022 5.26989746,6 C5.26889746,5.06573486 6.0041,3.25 8.0001,3.25 C9.7211,3.25 10.7501,4.648 10.7501,6 C10.7501,7.373 8.7589721,8.60931396 8.7589721,9 L8.7589721,9.5 Z M8.0001,13 C7.4481,13 7.0001,12.552 7.0001,12 C7.0001,11.448 7.4481,11 8.0001,11 C8.5521,11 9.0001,11.448 9.0001,12 C9.0001,12.552 8.5521,13 8.0001,13 M1.0001,8 C1.0001,11.866 4.1341,15 8.0001,15 C11.8661,15 15.0001,11.866 15.0001,8 C15.0001,4.134 11.8661,1 8.0001,1 C4.1341,1 1.0001,4.134 1.0001,8 Z"></path></svg>
+      </a>
+    </div>
+  </div>
+</template>
