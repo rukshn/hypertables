88cb1e341815f2b9997e46150d36f4fb60389592
Rukshan <arkruka@gmail.com>
2021-06-08 00:23:54 +0530

    Added table control component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <Navbar />
+    <TableControl :activeTab="0" :tables="tables" />
     <Grid />
   </div>
 </template>
@@ -9,12 +10,20 @@
 import { defineComponent } from 'vue'
 import Grid from './components/Grid.vue'
 import Navbar from './components/Navbar.vue'
+import TableControl from './components/TableControl.vue'
 
 export default defineComponent({
   name: 'App',
   components: {
     Grid,
-    Navbar
+    Navbar,
+    TableControl
+  },
+  data() {
+    const tables:string[] = ['Patients', 'Investigations']
+    return {
+      tables
+    }
   }
 })
 </script>
